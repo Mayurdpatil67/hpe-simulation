c3454820af4ca14b081b04b62ef8863143021078
Mayurdpatil67 <mayur.patil6911@gmail.com>
2025-04-11 13:13:46 +0530

    Employees class to hold the list of Employee objct

--- src/main/java/com/example/employee_service/Employees.java
+++ src/main/java/com/example/employee_service/Employees.java
@@ -3,7 +3,18 @@ package com.example.employee_service;
 import java.util.List;
 
 public class Employees {
+
     private List<Employee> employeeList;
 
+    public Employees(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
 
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
 }
