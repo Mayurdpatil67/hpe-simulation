d01a0a0a419f0fdeb540483486b0e40ed9e5b3d8
Mayurdpatil67 <mayur.patil6911@gmail.com>
2025-04-11 12:46:50 +0530

    intial commit

---
+++ src/main/java/com/example/employee_service/Employee.java
@@ -0,0 +1,38 @@
+package com.example.employee_service;
+
+public class Employee {
+
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String title;
+
+    public Employee(String employeeId, String firstName, String lastName, String email, String title) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.title = title;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}
---
+++ src/main/java/com/example/employee_service/Employees.java
@@ -0,0 +1,9 @@
+package com.example.employee_service;
+
+import java.util.List;
+
+public class Employees {
+    private List<Employee> employeeList;
+
+
+}
