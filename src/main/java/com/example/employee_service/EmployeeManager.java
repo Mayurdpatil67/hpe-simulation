25c6a9d5f3645e0a738876e6a65362bf05ce7c97
Mayurdpatil67 <mayur.patil6911@gmail.com>
2025-04-11 13:39:38 +0530

    EmployeeManager class with sample data./statically declared

---
+++ src/main/java/com/example/employee_service/EmployeeManager.java
@@ -0,0 +1,14 @@
+package com.example.employee_service;
+
+import java.util.Arrays;
+
+public class EmployeeManager {
+    public Employees getAllEmployees() {
+        return new Employees(Arrays.asList(
+                new Employee("001", "D", "R", "dr@example.com", "DA"),
+                new Employee("002", "M", "P", "mp@example.com", "SE"),
+                new Employee("003", "D", "P", "dp@example.com", "SE"),
+                new Employee("004", "R", "P", "rp@gmail.com", "BM")
+        ));
+    }
+}
