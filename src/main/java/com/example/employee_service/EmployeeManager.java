0323167df36a4204e25354b31794bd7e81b7cd30
Mayurdpatil67 <mayur.patil6911@gmail.com>
2025-04-12 16:35:28 +0530

    Updated EmployeeManager to add new employee using mutabel List

--- src/main/java/com/example/employee_service/EmployeeManager.java
+++ src/main/java/com/example/employee_service/EmployeeManager.java
@@ -2,16 +2,27 @@ package com.example.employee_service;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class EmployeeManager {
-    public Employees getAllEmployees() {
-        return new Employees(Arrays.asList(
-                new Employee("001", "D", "R", "dr@example.com", "DA"),
-                new Employee("002", "M", "P", "mp@example.com", "SE"),
-                new Employee("003", "D", "P", "dp@example.com", "SE"),
-                new Employee("004", "R", "P", "rp@gmail.com", "BM")
-        ));
+
+    private final List<Employee> employeeList = new ArrayList<>();
+
+    public EmployeeManager() {
+        employeeList.add(new Employee("001", "D", "R", "dr@example.com", "DA"));
+        employeeList.add(new Employee("002", "M", "P", "mp@example.com", "SE"));
+        employeeList.add(new Employee("003", "D", "P", "dp@example.com", "SE"));
+        employeeList.add(new Employee("004", "R", "P", "rp@gmail.com", "BM"));
     }
+
+    public Employees getAllEmployees() {
+        return new Employees(employeeList);
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
 }
