a1add0c184bb096857a710d037fb351b79dc4325
Mayurdpatil67 <mayur.patil6911@gmail.com>
2025-04-12 15:22:46 +0530

    Added default countructor for JSON desirialisation

--- src/main/java/com/example/employee_service/Employee.java
+++ src/main/java/com/example/employee_service/Employee.java
@@ -8,6 +8,10 @@ public class Employee {
     private String email;
     private String title;
 
+    //for the JSON deserialisation
+    public Employee() {
+    }
+
     public Employee(String employeeId, String firstName, String lastName, String email, String title) {
         this.employeeId = employeeId;
         this.firstName = firstName;
@@ -19,33 +23,52 @@ public class Employee {
     // It returns the string representation of the object
     @Override
     public String toString() {
-        return "Employee{" +
-                "email='" + email + '\'' +
-                ", employeeId='" + employeeId + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", title='" + title + '\'' +
-                '}';
+        return "Employee{" + "email='" + email + '\'' + ", employeeId='" + employeeId + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", title='" + title + '\'' + '}';
     }
 
     public String getEmployeeId() {
         return employeeId;
     }
 
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+
     public String getFirstName() {
         return firstName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
     public String getTitle() {
         return title;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 
 }
